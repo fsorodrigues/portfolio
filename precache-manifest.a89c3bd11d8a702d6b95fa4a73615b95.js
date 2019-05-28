60569cae16aee063dedac8a132777044192062f9
fsorodrigues <desouzaoliveirarod.f@husky.neu.edu>
2019-05-28 16:29:08 -0400

    init pop

---
+++ precache-manifest.a89c3bd11d8a702d6b95fa4a73615b95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adae983680e581ab04fde527034689f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29ac0e565ac4b71ade3c",
+    "url": "/static/css/2.d9debfcd.chunk.css"
+  },
+  {
+    "revision": "6bebaeeb7ef851721847",
+    "url": "/static/css/main.bd7f0e10.chunk.css"
+  },
+  {
+    "revision": "29ac0e565ac4b71ade3c",
+    "url": "/static/js/2.d899f64e.chunk.js"
+  },
+  {
+    "revision": "6bebaeeb7ef851721847",
+    "url": "/static/js/main.830ae7a1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
