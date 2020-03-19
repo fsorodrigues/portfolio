9fe80448ea2c0b7276aebd7395f737b2e084b648
fsorodrigues <desouzaoliveirarod.f@husky.neu.edu>
2020-03-20 01:05:56 +1300

    Update 2020

---
+++ precache-manifest.723cddf1ceb4517c3f77b398584e9180.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6cc6a6ca8cbd63726e304c9fea2cc8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1997c6202b34e5f46bf4",
+    "url": "/static/css/2.864de19f.chunk.css"
+  },
+  {
+    "revision": "be3738d068c01b1a2fe3",
+    "url": "/static/css/main.196c8e5e.chunk.css"
+  },
+  {
+    "revision": "1997c6202b34e5f46bf4",
+    "url": "/static/js/2.e0cf002e.chunk.js"
+  },
+  {
+    "revision": "66e94a3a78ed45fcf7bb75ede79d7a1f",
+    "url": "/static/js/2.e0cf002e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be3738d068c01b1a2fe3",
+    "url": "/static/js/main.d72695e7.chunk.js"
+  },
+  {
+    "revision": "c2f7d61519d2fd679c4f",
+    "url": "/static/js/runtime-main.476173c7.js"
+  }
+]);
