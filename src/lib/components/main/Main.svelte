b101e840f7d50006516a80d85fc76dfcf411698b
fsorodrigues <fsorodrigues@gmail.com>
2024-06-23 00:03:23 +1200

    Tweak typography

--- src/lib/components/main/Main.svelte
+++ src/lib/components/main/Main.svelte
@@ -1,5 +1,5 @@
-<main class="mt-20 lg:mt-40 mx-8 lg:mx-56">
-  <h1 class="text-3xl lg:text-5xl font-bold mb-6">
+<main class="mt-12 lg:mt-40 px-3 lg:px-56 py-4 flex-1 font-mono">
+  <h1 class="text-4xl lg:text-5xl font-bold mb-6">
     Hi! I'm Felippe Rodrigues. I create things with data.
   </h1>
   <p>Hit me up on <span>fsorodrigues [ at ] gmail</span> and let's have a chat.</p>
