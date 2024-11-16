c6dc685cbfc5b4ede2ad6dccf6e533143dfc8911
fsorodrigues <fsorodrigues@gmail.com>
2024-11-16 17:05:44 +1300

    feat: Add border to header link
    Change how header links align and behave for a better design cohesion. Now the copy will align perfectly to the baseline and the hover behavior is clearer.

--- src/lib/components/header/Header.svelte
+++ src/lib/components/header/Header.svelte
@@ -3,9 +3,11 @@
 </script>
 
 <header class='fixed top-0 left-0 right-0 h-12'>
-  <div class='flex items-center h-full py-1 px-3'>
+  <div class='flex items-center h-full py-1 px-3 font-mono'>
     <ToggleSwitch />
-    <p class="mr-6 border-b-4 border-primary-light-500 dark:border-primary-dark-500 font-mono">me</p>
-    <a href="https://blog.fsorodrigues.dev" class="mr-6 hover:opacity-90 hover:underline underline-offset-2 decoration-primary-light-500 dark:decoration-primary-dark-500 font-mono" >blog</a> 
+    <p class="mr-6 border-b-4 border-primary-light-500 dark:border-primary-dark-500">me</p>
+    <a 
+      href="https://blog.fsorodrigues.dev" 
+      class="mr-6 border-b-4 border-gray-200 hover:border-primary-light-500 dark:border-gray-800 hover:dark:border-primary-dark-500">blog</a> 
   </div>
 </header>
