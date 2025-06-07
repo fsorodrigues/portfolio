32b353594e7cf234fbeaa8a59e2131af834c04c7
fsorodrigues <fsorodrigues@gmail.com>
2025-06-07 13:20:40 +1200

    feat(home): home page w/ stack

--- src/lib/components/main/Main.svelte
+++ src/lib/components/main/Main.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+  import Stack from '$lib/components/stack/Stack.svelte';
+</script>
+
 <main class="mt-12 lg:mt-40 px-3 lg:px-44 xl:px-56 py-4 flex-1 font-mono">
   <div class="text-2xl sm:text-4xl xl:text-5xl font-bold mb-6 max-w-3xl">
     <h1>Hello! I'm Felippe.</h1> 
@@ -5,4 +9,7 @@
   </div>
 
   <p>Hit me up on <span>fsorodrigues [ at ] gmail</span> and let's have a chat.</p>
+    
+  <Stack />
 </main>
+
