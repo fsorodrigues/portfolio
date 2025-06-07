d033de2d526e02506b5843d7fe436e168042fe62
fsorodrigues <fsorodrigues@gmail.com>
2025-06-07 13:21:35 +1200

    feat(wrapper): reusable component for consistent position

---
+++ src/lib/components/page-wrapper/Wrapper.svelte
@@ -0,0 +1,7 @@
+<script>
+ const { children } = $props()
+</script>
+
+<main class="mt-12 lg:mt-40 px-3 lg:px-10 xl:px-56 py-4 flex-1 flex flex-col gap-y-12 font-mono">
+  {@render children()}
+</main>
