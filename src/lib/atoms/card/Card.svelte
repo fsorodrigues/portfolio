d034e44d8fe392b9f483b55776cef97b8d28d55e
fsorodrigues <fsorodrigues@gmail.com>
2025-06-07 16:18:30 +1200

    feat: tweaks for super big screens

--- src/lib/atoms/card/Card.svelte
+++ src/lib/atoms/card/Card.svelte
@@ -8,13 +8,13 @@
 <a 
   class="flex flex-col justify-between gap-3 relative overflow-hidden 
   border-3 border-primary-light-100 dark:border-primary-dark-100 
-  {item.award ? 'bg-gray-300 dark:bg-gray-600' : ''} h-80
+  {item.award ? 'bg-gray-300 dark:bg-gray-700' : ''} h-80 2xl:h-[500px]
   opacity-90 hover:opacity-100 z-20"
   href={item.link}
 >
-  <div class="flex-grow flex-shrink-0  sm:max-h-40 md:max-h-48">
+  <div class="flex-grow flex-shrink-0 sm:max-h-40 md:max-h-48 2xl:max-h-96">
     <div
-      class="h-full bg-cover "
+      class="h-full bg-cover bg-center bg-gray-50 dark:bg-gray-950"
       style="background-image: url('https://pocketbase.fsorodrigues.dev/api/files/fqgnykni196i3b7/{item.id}/{item.image}')"
     ></div>
   </div>
@@ -22,8 +22,8 @@
     {item.title} 
   </p>
   {#if item.award === true}
-    <div class="absolute -top-12 -right-12 w-24 h-24 rotate-45 bg-primary-light-100 dark:bg-primary-dark-50"></div>
-    <div class="absolute top-0 right-0 w-10 h-10">
+    <div class="absolute -top-12 -right-12 2xl:-top-24 2xl:-right-24 w-24 h-24 2xl:w-48 2xl:h-48 rotate-45 bg-primary-light-100 dark:bg-primary-dark-50"></div>
+    <div class="absolute top-0 right-0 2xl:top-3 2xl:right-3 w-10 h-10 2xl:w-12 2xl:h-12">
       <Award />
     </div>
   {/if}
--- src/lib/icons/Award.svelte
+++ src/lib/icons/Award.svelte
@@ -1,6 +1,6 @@
 <div class="info-icon absolute flex items-center justify-center top-0 left-0 h-full w-full pointer-events-none fill-primary-dark-300 stroke-primary-dark-600">
   <!-- https://feathericons.dev/?search=info&iconset=feather -->
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-award">
+  <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-award">
     <polyline points="8.21 13.89 7 23 12 20 17 23 15.79 13.88"></polyline>
     <circle cx="12" cy="8" r="7"></circle>
   </svg>
